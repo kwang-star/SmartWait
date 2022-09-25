17d33911105c0994365eaafb120cc4804a81486a
Wang <kmely.wang@wabtec.com>
2022-09-25 19:33:59 -0400

    US2:Create New Patient, Initial commit of link js to button

---
+++ src/register_user.js
@@ -0,0 +1,7 @@
+'use strict';
+const regBut = document.querySelector('.btn[id="regUser"');
+
+regBut.addEventListener('click', function() {
+    console.log("Button:" + this.textContent + ' was clicked');
+    this.textContent += " Clicked";
+});
